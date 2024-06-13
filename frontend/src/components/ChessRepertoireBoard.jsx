15e92ad84f073328cb648dbfd2f0a98078d02d96
Cismaticowebdev <cismaticowebdev@gmail.com>
2024-06-13 12:45:08 +0100

    Add board rotation functionality

--- frontend/src/components/ChessRepertoireBoard.jsx
+++ frontend/src/components/ChessRepertoireBoard.jsx
@@ -1,6 +1,17 @@
+import { Chessboard } from "react-chessboard";
+import { useState } from "react";
+
 function ChessRepertoireBoard() {
+    const [rotateBoard, setRotateBoard] = useState("white");
+
+    function handleRotateBoard() {
+        setRotateBoard(rotateBoard === "white" ? "black" : "white");
+    }
+
     return <div>
         <h1>Chess Board</h1>
+        <Chessboard boardWidth={520} boardOrientation={rotateBoard}/>
+        <button onClick={handleRotateBoard}>Rotate Board</button>
     </div>
 }
 
--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,6 +1,10 @@
+
+import ChessRepertoireBoard from "../components/ChessRepertoireBoard";
+
 function Home() {
     return <div>
         <h1>Home</h1>
+        <ChessRepertoireBoard />
     </div>
 }
 
