3c2033bb0e8a446795e0e3a7f085aabe0add6d38
Cismaticowebdev <cismaticowebdev@gmail.com>
2024-06-13 14:03:56 +0100

    Add move pieces functionality and only legal moves

--- frontend/src/components/ChessRepertoireBoard.jsx
+++ frontend/src/components/ChessRepertoireBoard.jsx
@@ -1,8 +1,26 @@
 import { Chessboard } from "react-chessboard";
+import { Chess } from "chess.js";
 import { useState } from "react";
 
 function ChessRepertoireBoard() {
+    const chess = new Chess();
+
     const [rotateBoard, setRotateBoard] = useState("white");
+    const [boardPosition, setBoardPosition] = useState("start");
+    const [gamePGN, setGamePGN] = useState(chess.pgn());
+
+    function onDrop(sourceSquare, targetSquare) {
+        chess.loadPgn(gamePGN);
+        const move = {
+          from: sourceSquare,
+          to: targetSquare,
+          promotion: "q",
+        };
+
+        chess.move(move);
+        setBoardPosition(chess.fen());
+        setGamePGN(chess.pgn());
+    }
 
     function handleRotateBoard() {
         setRotateBoard(rotateBoard === "white" ? "black" : "white");
@@ -10,7 +28,7 @@ function ChessRepertoireBoard() {
 
     return <div>
         <h1>Chess Board</h1>
-        <Chessboard boardWidth={520} boardOrientation={rotateBoard}/>
+        <Chessboard boardWidth={520} animationDuration={0} boardOrientation={rotateBoard} position={boardPosition} onPieceDrop={onDrop} />
         <button onClick={handleRotateBoard}>Rotate Board</button>
     </div>
 }
