2ab789a03e804c592f549db6afe9d7addf1e4364
Cismaticowebdev <cismaticowebdev@gmail.com>
2024-07-25 17:36:57 +0100

    Add GET method in frontend, cors error

--- frontend/src/components/MyRepertoiresList.jsx
+++ frontend/src/components/MyRepertoiresList.jsx
@@ -1,6 +1,34 @@
+import { useState, useEffect } from "react";
+
 function MyRepertoiresList() {
+const [repertoires, setRepertoires] = useState([]);
+
+useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await fetch('http://localhost:3000/api/data');
+            const data = await response.json();
+            setRepertoires(data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    fetchData();
+}, []);
+
     return <div>
         <h1>List</h1>
+        <ul>
+            {repertoires.map(repertoire => (
+                <li>
+                    key={repertoire.id}
+                    title={repertoire.title}
+                    moves={repertoire.moves}
+                    creator={repertoire.creator}
+                </li>
+            ))}
+        </ul>
     </div>
 }
 
